6962aea91c53a0bbc3bc2c9754cbfb38db448e23
Ishtahad Hossain <ishtahad@gmail.com>
2024-05-19 17:38:52 +0600

    Time: 17 ms (17.30%), Space: 45.1 MB (23.94%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,13 +3,13 @@ class Solution {
         HashMap<Character,Integer> countMagazine = new HashMap<>();
             HashMap<Character,Integer> countRansomnote = new HashMap<>();
 
-            for(int i = 0; i<ransomNote.length();i++){
+            for(int i = 0; i<ransomNote.length();i++)
                 countRansomnote.put(ransomNote.charAt(i),countRansomnote.getOrDefault(ransomNote.charAt(i),0) + 1);
-            }
+            
 
-            for(int i = 0; i<magazine.length();i++){
+            for(int i = 0; i<magazine.length();i++)
                 countMagazine.put(magazine.charAt(i),countMagazine.getOrDefault(magazine.charAt(i),0) + 1);
-            }
+            
 
             for (Map.Entry<Character,Integer> mapElement : countRansomnote.entrySet()) {
                 Character key = mapElement.getKey();
