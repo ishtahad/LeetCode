8b63dde04b625a9f6022cb55e79497623af12e6a
Ishtahad Hossain <ishtahad@gmail.com>
2024-07-14 22:51:24 +0600

    Time: 12 ms (43.9%), Space: 42.5 MB (21.55%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -6,31 +6,31 @@ class Solution {
         HashMap<Character, Character> map = new HashMap<>();
 
         for(int i = 0 ; i < s.length() ; i++){
-                System.out.println(" Trying to replace s: " + s.charAt(i) + " with a character of t: " + t.charAt(i));
-                System.out.println(" First checking if character of s has already been replaced :" + map.containsKey(s.charAt(i)));
+                //System.out.println(" Trying to replace s: " + s.charAt(i) + " with a character of t: " + t.charAt(i));
+                //System.out.println(" First checking if character of s has already been replaced :" + map.containsKey(s.charAt(i)));
                    if ( map.containsKey(s.charAt(i))){
-                       System.out.println("character of s : " + s.charAt(i) + " has already been replaced with : "  + map.get(s.charAt(i)));
-                       System.out.println("Now character of t that wants to replace  : " + t.charAt(i));
-                       System.out.println("t need to be same as already replaced character : " + (map.get(s.charAt(i)) == t.charAt(i)));
+                       //System.out.println("character of s : " + s.charAt(i) + " has already been replaced with : "  + map.get(s.charAt(i)));
+                       //System.out.println("Now character of t that wants to replace  : " + t.charAt(i));
+                       //System.out.println("t need to be same as already replaced character : " + (map.get(s.charAt(i)) == t.charAt(i)));
 
                        if (map.get(s.charAt(i)) == t.charAt(i)){
-                        System.out.println("because it is same so we continue");
-                        System.out.println("");
+                        //System.out.println("because it is same so we continue");
+                        //System.out.println("");
                         continue;
                        }else{
-                        System.out.println("because it is not same so we return false");
+                        //System.out.println("because it is not same so we return false");
                         return false;
                        }
                    }else if(map.containsValue(t.charAt(i))){
-                       System.out.print("Now i found a character of t : " + t.charAt(i) + " which has already been used to replace ");
+                       //System.out.print("Now i found a character of t : " + t.charAt(i) + " which has already been used to replace ");
                        Character key = null;
                        for (Map.Entry<Character, Character> entry : map.entrySet()) {
                             if (entry.getValue().equals(t.charAt(i))) {
                                 key = entry.getKey();
                             }
                         }
-                        System.out.print(key);
-                        System.out.println("Now i need to check if i am not trying to replace a different character");
+                        //System.out.print(key);
+                        //System.out.println("Now i need to check if i am not trying to replace a different character");
                         if(key == s.charAt(i)) continue;
                         else return false;
 
@@ -39,8 +39,8 @@ class Solution {
                    
                    else{
 
-                    System.out.println("replacing s : " + s.charAt(i) + " with t : " + t.charAt(i));
-                    System.out.println("");
+                    //System.out.println("replacing s : " + s.charAt(i) + " with t : " + t.charAt(i));
+                    //System.out.println("");
                     map.put(s.charAt(i),t.charAt(i));
                    } 
             
