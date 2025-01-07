e02e291a7a5602e7f4c69dcf7480d620f0e13708
Ishtahad Hossain <ishtahad@gmail.com>
2025-01-07 18:43:36 +0600

    Time: 4 ms (86.21%), Space: 42.3 MB (37.89%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -1,20 +1,24 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
-        List<String> ans = new ArrayList();
-        int length = words.length;
-          for (int i = 0 ; i < length ; i++) {
-            String sub = words[i];
-              for (int j = 0 ; j < length ; j++) {
-                 if (i!=j) {
-                    String text = words[j];
-                     if (text.contains(sub)){
-                        if (!ans.contains(sub)) {
-                               ans.add(sub);
-                        }
-                     }
-                 }
-              }
-          }
-           return ans;
+    // Use a Set to avoid duplicate substrings
+    Set<String> resultSet = new HashSet<>();
+    
+    // Sort the words array based on string lengths
+    Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
+    
+    int length = words.length;
+    for (int i = 0; i < length; i++) {
+        String sub = words[i];
+        for (int j = i + 1; j < length; j++) { // Compare only with longer strings
+            String text = words[j];
+            if (text.contains(sub)) {
+                resultSet.add(sub);
+                break; // No need to check further for this substring
+            }
+        }
     }
+    
+    // Convert the Set to a List and return
+    return new ArrayList<>(resultSet);
+}
 }
