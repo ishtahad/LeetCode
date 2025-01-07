5a1a523fce280dc0cda6ae54785ae0e522a7450c
Ishtahad Hossain <ishtahad@gmail.com>
2025-01-07 18:38:10 +0600

    Time: 5 ms (41.61%), Space: 42.4 MB (37.89%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -3,10 +3,12 @@ class Solution {
         Set<String> ans = new HashSet<>();
         int length = words.length;
           for (int i = 0 ; i < length ; i++) {
+            String sub = words[i];
               for (int j = 0 ; j < length ; j++) {
                  if (i!=j) {
-                     if (words[j].contains(words[i])){
-                         ans.add(words[i]);
+                    String text = words[j];
+                     if (text.contains(sub)){
+                         ans.add(sub);
                      }
                  }
               }
