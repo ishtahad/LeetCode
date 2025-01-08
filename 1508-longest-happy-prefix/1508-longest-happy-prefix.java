e3ba4f90bb01d3153dd3fefbff7b8f7da01d56ae
Ishtahad Hossain <ishtahad@gmail.com>
2025-01-09 03:40:58 +0600

    Time: 9 ms (92.59%), Space: 44.7 MB (100%) - LeetHub

--- 1508-longest-happy-prefix/1508-longest-happy-prefix.java
+++ 1508-longest-happy-prefix/1508-longest-happy-prefix.java
@@ -1,20 +1,20 @@
 class Solution {
     public String longestPrefix(String str) {
-        int[] lps = new int[str.length()];
+        int length = str.length();
+        int[] lps = new int[length];
         int len = 0;
         lps[0] = 0;
 
         int i = 1;
-        while (i < str.length()) {
+        while (i < length) {
             if (str.charAt(i) == str.charAt(len)) {
                 len++;
                 lps[i] = len;
                 i++;
             }
             else {
-                if (len != 0) {
+                if (len != 0) 
                     len = lps[len - 1];
-                }
                 else {
                     lps[i] = 0;
                     i++;
@@ -22,6 +22,6 @@ class Solution {
             }
         }
 
-        return str.substring(0,lps[str.length()-1]);
+        return str.substring(0,lps[length-1]);
     }
 }
