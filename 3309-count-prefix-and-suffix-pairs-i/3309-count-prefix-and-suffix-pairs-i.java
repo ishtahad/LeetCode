773030ff8da797a075a6f493ba089d71ab93987b
Ishtahad Hossain <ishtahad@gmail.com>
2025-01-09 02:02:16 +0600

    Time: 2 ms (100%), Space: 42.6 MB (39.96%) - LeetHub

--- 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.java
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.java
@@ -3,7 +3,7 @@ class Solution {
         int count = 0;
         for (int i = 0 ; i < words.length - 1 ; i++) {
             for (int j = i+1 ; j < words.length ; j++) {
-                if (isPrefixAndSuffix(words[i],words[j])) {
+                if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) {
                     count++;
                 }
             }
@@ -12,19 +12,21 @@ class Solution {
     }
 
     private boolean isPrefixAndSuffix(String str1, String str2) {
-        int lengthPat = str1.length();
-        int lengthText = str2.length();
+        //int lengthPat = str1.length();
+        //int lengthText = str2.length();
 
-        if (lengthPat > lengthText) {
-            return false;
+        //if (lengthPat > lengthText) {
+          //  return false;
+        //}
+
+        if (str2.startsWith(str1) && str2.endsWith(str1)){
+            return true;
         }
-
+        //if (!str2.substring(0,lengthPat).equals(str1)) return false;
         
-        if (!str2.substring(0,lengthPat).equals(str1)) return false;
-        
-        if (!str2.substring(lengthText-lengthPat,lengthText).equals(str1)) return false;
+        //if (!str2.substring(lengthText-lengthPat,lengthText).equals(str1)) return false;
 
-        return true;
+        return false;
            
     } 
 }
