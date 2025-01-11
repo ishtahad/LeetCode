b2e01a4360bae86ed4aa7163c83e4b1750650a29
Ishtahad Hossain <ishtahad@gmail.com>
2025-01-12 01:30:07 +0600

    Time: 5 ms (66.55%), Space: 45.5 MB (45.32%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -10,13 +10,10 @@ class Solution {
 
         int counter = 0;
         for (int i = 0 ; i < 26 ; i++) {
-            System.out.println("count: " + count[i]);
             if (count[i] % 2 != 0) {
                 counter = counter + (count[i] % 2);
             }
         }
-        System.out.println(counter);
-
         if (counter > k ) return false;
         return true;
     }
