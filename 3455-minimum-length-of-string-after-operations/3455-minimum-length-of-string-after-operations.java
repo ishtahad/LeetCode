03c7f923cc19688ae18c64f58b5b6c28bf5ba08c
Ishtahad Hossain <ishtahad@gmail.com>
2025-01-14 02:14:18 +0600

    Time: 13 ms (54%), Space: 46.4 MB (54.67%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,10 +1,10 @@
 class Solution {
     public int minimumLength(String s) {
+        int answer = 0;
         int[] count = new int[26];
         for (Character c : s.toCharArray()) {
-            count[c- 'a']++;
+            count[c-'a']++;
         }
-        int answer = 0;
         for (int freq : count) {
             if (freq > 0) {
                 answer += freq % 2 == 0 ? 2 : 1;
