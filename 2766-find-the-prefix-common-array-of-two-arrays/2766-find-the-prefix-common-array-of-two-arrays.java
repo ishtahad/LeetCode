ad4fe5582f8de783e80a25c952d033ac63743a0c
Ishtahad Hossain <ishtahad@gmail.com>
2025-01-15 01:50:05 +0600

    Time: 1 ms (100%), Space: 45.5 MB (76.84%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.java
@@ -5,12 +5,8 @@ class Solution {
         int count= 0;
         for (int i = 0 ; i < A.length; i++) {
             isPresent[A[i]]++;
-            //System.out.println("A[i] : " + A[i]);
-            //System.out.println("isPresent[A[i]]: " + isPresent[A[i]]);
             if (isPresent[A[i]]==2) count++;
             isPresent[B[i]]++;
-            //System.out.println("B[i] : " + A[i]);
-            //System.out.println("isPresent[B[i]]: " +isPresent[B[i]]);
             if(isPresent[B[i]]==2) count++;
             result[i] = count;
         }
