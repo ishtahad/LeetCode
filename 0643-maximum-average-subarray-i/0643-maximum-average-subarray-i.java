c80408d813daec6d7e7d3ab3956710743eb879b4
Ishtahad Hossain <ishtahad@gmail.com>
2025-02-26 00:31:50 +0600

    Time: 2 ms (99.92%), Space: 56.6 MB (33.94%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,5 +1,7 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
+        // Brute force
+        /*
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i <= nums.length - k; i++) {
             int sum = 0;
@@ -9,5 +11,20 @@ class Solution {
             maxSum = Math.max(maxSum, sum);
         }
         return (double)maxSum/k;
+        */
+        // Optimal Sliding window
+        int windowSum = 0, maxSum = 0;
+        
+        for (int i = 0; i < k; i++) {
+            windowSum += nums[i];
+        }
+        maxSum = windowSum;
+
+        for (int i = k; i < nums.length; i++) {
+            windowSum += nums[i] - nums[i - k]; 
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        return (double)maxSum/k;
+
     }
 }
