c73d16f09b2ec18172e789433389784116c5ed05
Ishtahad Hossain <ishtahad@gmail.com>
2026-08-23 20:52:25 +0600

    Time: 22 ms (7.76%), Space: 53.6 MB (6.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String,List<String>> map = new HashMap<>();
-        String[] sorted = new String[strs.length];
-        for(int i = 0 ; i < strs.length;i++){
-            char[] charArray1 = strs[i].toCharArray();
-            Arrays.sort(charArray1);
-            sorted[i] = new String(charArray1);
+        List<List<String>> result = new ArrayList<>();
+        HashMap<String ,List<String>> strCountMap = new HashMap<>();
+        for (String str : strs) {
+           HashMap<String ,String> sortStr = sortStr(str);
+           if (strCountMap.containsKey(sortStr.get(str))) {
+               strCountMap.get(sortStr.get(str)).add(str);
+           } else {
+               List<String> newList = new ArrayList<>();
+               newList.add(str);
+               strCountMap.put(sortStr.get(str), newList);
+           }
         }
-        for(int i = 0 ; i < sorted.length;i++){
-            String a = sorted[i];
-            if(!map.containsKey(a)){
-                map.put(a,new ArrayList<>());
-            }
-            map.get(a).add(strs[i]);
-        }
-        return new ArrayList<>(map.values());
+        result.addAll(strCountMap.values());
+        return result;
     }
 
-
-    
+    private  HashMap<String ,String> sortStr(String str) {
+        HashMap<String ,String> sortedStr = new HashMap<>();
+        sortedStr.put(str, str.chars().sorted().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString());
+        return sortedStr;
+    }
 }
