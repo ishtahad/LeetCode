e635bda406a0dcef08205246e092e46290b1918c
Ishtahad Hossain <ishtahad@gmail.com>
2026-09-05 00:45:43 +0600

    Time: 19 ms (48.96%), Space: 47.5 MB (81.73%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.java
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.java
@@ -1,22 +1,21 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
-           int bestNum = 100000;
-           int bestCount = -9999;
+           int bestNum = 10000000;
+           int bestCount = 0;
            HashMap<Integer, Integer> countMap = new HashMap<>();
 
-           for (int i = 0 ; i < nums.length ; i++) {
-               if (nums[i] % 2 ==0) {
-                    countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
+           for (int num : nums) {
+               if (num% 2 ==0) {
+                    countMap.put(num, countMap.getOrDefault(num, 0) + 1);
 
-                    if (bestCount < countMap.get(nums[i])) {
-                        bestNum = nums[i];
-                        bestCount = countMap.get(nums[i]);
-                    } else if (bestCount == countMap.get(nums[i]) && bestNum > nums[i]) {
-                        bestNum = nums[i];
+                    if (bestCount < countMap.get(num)) {
+                        bestNum = num;
+                        bestCount = countMap.get(num);
+                    } else if (bestCount == countMap.get(num) && bestNum > num) {
+                        bestNum = num;
                     }
                }
            }
-
-           return bestNum == 100000 ? -1 : bestNum;
+           return bestCount == 0 ? -1 : bestNum;
     }
 }
